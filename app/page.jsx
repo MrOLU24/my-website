7a5f79f1460b1a7b2a8e560792aae8ce86c1ae5c
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-04-22 13:57:34 +0100

    set up shadcN

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,6 @@
-import Image from 'next/image'
+// This is a Next.js 13+ app directory example
+// Importing the necessary CSS file for global styles
+import './globals.css'
 
 export default function Home() {
   return (
