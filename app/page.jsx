8dfce45d588a696e98eb908d99705627aaf807df
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-04-29 12:33:48 +0100

    portfolio image

--- app/page.jsx
+++ app/page.jsx
@@ -4,13 +4,14 @@ import "./globals.css";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Socials from "@/components/socials";
+import Photo from "@/components/Photo";
 
 export default function Home() {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I&apos;m <br />
@@ -37,7 +38,9 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div>photo</div>
+          <div>
+              <Photo className="order-1 xl:order-none mb-8 xl:mb-0"/>
+          </div>
         </div>
       </div>
     </section>
