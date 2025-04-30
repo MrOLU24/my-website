74c2663f5021c2a2078bc1036f6baea8a8a959eb
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-04-30 23:17:18 +0100

    stats

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Socials from "@/components/socials";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 export default function Home() {
   return (
@@ -43,6 +44,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <Stats/>
     </section>
   );
 }
