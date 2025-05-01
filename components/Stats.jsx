4abc82c0d49261d4db938757859896e5e65ca742
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-05-01 13:04:23 +0100

    stats features

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+"use client";
+
+import CountUp from "react-countup";
+
+const stats = [
+  { num: 2, text: "Years of Experience" },
+  { num: 8, text: "Projects Completed" },
+  { num: 2, text: "Technologies mastered" },
+  { num: 1000, text: "code commits" },
+];
 
 const Stats = () => {
   return (
-    <div>Stats</div>
-  )
-}
+    <section>
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-y-6 max-w-[80vw] mx-auto xl:max-w-none">
+          {stats.map((item, index) => (
+            <div
+              key={index}
+              className="w-1/2 md:w-1/3 xl:flex-1 flex gap-4 items-center justify-center xl:justify-start px-2"
+            >
+              <CountUp
+                end={item.num}
+                duration={5}
+                delay={2}
+                className="text-3xl md:text-4xl xl:text-6xl font-extrabold"
+              />
+              <p
+                className={`${
+                  item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                } text-sm md:text-base`}
+              >
+                {item.text}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Stats
+export default Stats;
