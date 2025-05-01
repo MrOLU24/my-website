290329ee222efe13d3fadae1160b4a05b57c50c0
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-05-01 13:59:29 +0100

    reset interval

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import CountUp from "react-countup";
 
 const stats = [
@@ -10,25 +11,35 @@ const stats = [
 ];
 
 const Stats = () => {
+  const [resetKey, setResetKey] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setResetKey((prev) => prev + 1); // Triggers re-animation
+    }, 7000); // Matches count duration (5s) + delay
+
+    return () => clearInterval(interval); // Clean up
+  }, []);
+
   return (
-    <section>
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-y-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => (
             <div
-              key={index}
+              key={`${index}-${resetKey}`}
               className="w-1/2 md:w-1/3 xl:flex-1 flex gap-4 items-center justify-center xl:justify-start px-2"
             >
               <CountUp
                 end={item.num}
                 duration={5}
-                delay={2}
+                delay={0.5}
                 className="text-3xl md:text-4xl xl:text-6xl font-extrabold"
               />
               <p
                 className={`${
                   item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
-                } text-sm md:text-base`}
+                } text-sm md:text-base leading-snug text-white/80`}
               >
                 {item.text}
               </p>
