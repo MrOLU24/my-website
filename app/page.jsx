f8e774792bcc645cd10751e1d35c7e44d7177cf1
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-05-03 11:26:27 +0100

    services

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,7 @@ export default function Home() {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I&apos;m <br />
+              <span className="text-sm">Hello I&apos;m <br /></span>
               <span className="text-accent-solid">Olatunji Oluwashola</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,6 +1,67 @@
+"use client";
 
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const serviceItems = [
+  {
+    num: 1,
+    title: "Web Development",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    href: "",
+  },
+  {
+    num: 2,
+    title: "Mobile Development",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    href: "",
+  },
+  {
+    num: 3,
+    title: "Vidio Editor",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    href: "",
+  },
+  {
+    num: 4,
+    title: "Digital Marketing",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    href: "",
+  },
+];
 const Services = () => {
-  return <div> Services page</div>;
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0 ">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 2.4, duration: 0.4, ease: "easeIn" }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {serviceItems.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-col flex-1 justify-center gap-6 group"
+            >
+              <div className="w-full flex items-center justify-between">
+                <div className="text-6xl font-extrabold text-outline text-transparent">
+                  {item.num}
+                </div>
+                <Link href={item.href}>
+                  <BsArrowDownRight />
+                </Link>
+              </div>
+              <h2>{item.title}</h2>
+              <p>{item.desc}</p>
+              <div className="border-b border-white/20 w-full"></div>
+            </div>
+          ))}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
