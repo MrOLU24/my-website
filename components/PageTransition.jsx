948ad99671c6e46ba701502879ca225d80e1a961
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-05-12 00:50:07 +0100

    still fixing

--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,24 +1,21 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 
 const PageTransition = ({ children }) => {
   const pathname = usePathname();
+
   return (
-    <AnimatePresence>
-      <div key={pathname}>
-        <motion.div
-          className="h-screen w-screen fixed bg-primary-solid top-0 pointer-events-none"
-          initial={{ opacity: 1 }}
-          animate={{
-          opacity: 0,
-          transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
-          }}
-        ></motion.div>
-        {children}
-      </div>
-    </AnimatePresence>
+    <motion.div
+      key={pathname}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.div>
   );
 };
 
