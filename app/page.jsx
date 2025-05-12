768f69bd7fa338cb27d66248c1924e339b249596
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-05-12 02:03:25 +0100

    download cv

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,9 @@ export default function Home() {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              <span className="text-sm">Hello I&apos;m <br /></span>
+              <span className="text-sm">
+                Hello I&apos;m <br />
+              </span>
               <span className="text-accent-solid">Olatunji Oluwashola</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
@@ -23,14 +25,21 @@ export default function Home() {
               proficient to various programming languages and technologies
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
+              <a
+                href="/resume.pdf"
+                download
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                <span>Download CV</span>
-                <FiDownload />
-              </Button>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
@@ -44,7 +53,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <Stats/>
+      <Stats />
     </section>
   );
 }
