d17e1ad6288805f00678b0395216947ab820c2b7
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-05-20 23:13:36 +0100

    fixing about responsiveness

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -178,11 +178,15 @@ const Resume = () => {
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
                   {about.description}
                 </p>
-                <ul className="grid grid-colos-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0 text-left">
                   {about.info.map((item, index) => (
-                    <li key={index} className="flex items-center justify-center xl:justify-start gap-4">
-                      <span className="text-white/60">{item.fieldName}</span>
-                      <span className="text-xl">{item.fieldValue}</span>
+                    <li key={index} className="flex items-center gap-4">
+                      <span className="text-white/60 min-w-[90px]">
+                        {item.fieldName}
+                      </span>
+                      <span className="text-xl break-all">
+                        {item.fieldValue}
+                      </span>
                     </li>
                   ))}
                 </ul>
