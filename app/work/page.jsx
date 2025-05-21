13af57316ae882e5d1fb868c16ff9334c6891b33
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-05-21 19:51:54 +0100

    swipper added

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -61,7 +61,10 @@ const Work = () => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn"} }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
@@ -136,7 +139,10 @@ const Work = () => {
                   </div>
                 </SwiperSlide>
               ))}
-               <WorkSliderBtn/>
+              <WorkSliderBtn
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent-solid hover:bg-accent-hover text-primary-solid text-[22px] w-[44px] flex justify-center items-center transition-all duration-500"
+              />
             </Swiper>
           </div>
         </div>
