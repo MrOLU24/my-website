0e265a5b46eba903fe96c545f0e52c64d93e5a02
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-05-22 23:39:32 +0100

    handle the links

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -4,41 +4,32 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
 
 const Links = [
-  {
-    name: "home",
-    path: "/",
-  },
-  {
-    name: "services",
-    path: "/services",
-  },
-  {
-    name: "resume",
-    path: "/resume",
-  },
-  {
-    name: "work",
-    path: "/work",
-  },
-  {
-    name: "contact",
-    path: "/contact",
-  },
+  { name: "home", path: "/" },
+  { name: "services", path: "/services" },
+  { name: "resume", path: "/resume" },
+  { name: "work", path: "/work" },
+  { name: "contact", path: "/contact" },
 ];
 
 const MobileNav = () => {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setOpen(false); // Close the menu no matter which link is clicked
+  };
 
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-accent-solid" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         <div className="mt-32 mb-40 text-center text-2xl">
-          <Link href="/">
+          <Link href="/" onClick={handleLinkClick}>
             <h1 className="text-4xl font-semibold">
               MrOLU <span className="text-accent-solid">.</span>
             </h1>
@@ -49,9 +40,11 @@ const MobileNav = () => {
             <Link
               key={index}
               href={link.path}
+              onClick={handleLinkClick}
               className={`${
-                link.path === pathname &&
-                "text-accent-solid border-b-2 border-accent-solid"
+                link.path === pathname
+                  ? "text-accent-solid border-b-2 border-accent-solid"
+                  : ""
               } text-xl capitalize hover:text-accent-hover transition-all`}
             >
               {link.name}
