4e5398cb33a919d12d17b1fc2e6c6c22c68cace6
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-05-22 13:28:59 +0100

    button implement

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -45,7 +45,7 @@ const Contact = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          <div className="xl:h-[54%] order-2 xl:order-none ">
+          <div className="xl:w-[54%] order-2 xl:order-none ">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-accent-solid text-4xl">
                 Let's work together
@@ -75,10 +75,25 @@ const Contact = () => {
                 </SelectContent>
               </Select>
               <Textarea className="h-[200px]" placeholder="Your message here" />
+              <Button size="md" className="max-w-48">
+                Send message
+              </Button>
             </form>
           </div>
           <div className="flex-1 items-center flex xl:justify-end order-1 xl:order-none mb-8 xl:mb-3">
-            info
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => (
+                <li key={index} className="flex items-center gap-6">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent-solid rounded-md flex items-center justify-center">
+                    <div className="text-[28px]">{item.icon}</div>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-white/60">{item.title}</p>
+                    <h3 className="text-xl">{item.desc}</h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
