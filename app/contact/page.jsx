114ca0019f7deebe8d5b9e1406c7ba15b1067980
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-05-22 12:53:20 +0100

    input and select styled

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -50,15 +50,31 @@ const Contact = () => {
               <h3 className="text-accent-solid text-4xl">
                 Let's work together
               </h3>
-              <p>
-                I'm interested in collaborating on your next project. Please fill out the form below and I'll get back to you as soon as possible.
+              <p className="text-white/60">
+                I'm interested in collaborating on your next project. Please
+                fill out the form below and I'll get back to you as soon as
+                possible.
               </p>
-              <div>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <Input type="firstName" placeholder="First Name" />
                 <Input type="lastName" placeholder="Last Name" />
-                <Input type="email" placeholder="Email" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone number" />
                 <Input type="message" placeholder="Message" />
               </div>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a Service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">Digital Marketing</SelectItem>
+                    <SelectItem value="mst">Video Editor</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             </form>
           </div>
           <div className="flex-1 items-center flex xl:justify-end order-1 xl:order-none mb-8 xl:mb-3">
